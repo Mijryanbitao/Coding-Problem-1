7e5e021a3364b51e55cfd112504ca9fbd4171819
Mijryanbitao <mijryanb@gmail.com>
2024-11-04 17:48:26 +0800

    Create userProfile.js

---
+++ userProfile.js
@@ -0,0 +1,9 @@
+let fullName = "Mij Ryan Bitao";
+let age = 20;
+let favoriteNumber = 09;
+let favoriteColor = "Black";
+
+console.log(fullName);
+console.log(age);
+console.log(favoriteNumber);
+console.log(favoriteColor);
